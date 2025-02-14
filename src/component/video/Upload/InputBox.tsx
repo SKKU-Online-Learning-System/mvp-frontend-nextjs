6d4aab8b99ee7a032aef402845fc811db290a1db
yeobi01 <yeobi_01@naver.com>
2025-02-14 18:21:11 +0900

    feat: Modal 컴포넌트 퍼블리싱

---
+++ src/component/video/Upload/InputBox.tsx
@@ -0,0 +1,21 @@
+type Props = {
+  label: string;
+  placeholder: string;
+  setData: (data: string) => void;
+};
+
+export function InputBox({ label, placeholder, setData }: Props) {
+  return (
+    <div className="flex h-20 w-full flex-col justify-between rounded-xl border-[1.5px] border-gray-300 px-4 py-3 focus-within:border-green-700">
+      <p className="text-[0.9rem] text-gray-500">{label}</p>
+      <input
+        type="text"
+        className="w-full border-0 outline-none"
+        placeholder={placeholder}
+        onChange={(e) => {
+          setData(e.target.value);
+        }}
+      />
+    </div>
+  );
+}
