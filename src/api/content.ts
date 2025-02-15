7bf796837582d83ea954b5f2df8f1eaf0b89c06d
yeobi01 <yeobi_01@naver.com>
2025-02-15 18:03:43 +0900

    add: type에 실수로 [] 빼먹음

--- src/api/content.ts
+++ src/api/content.ts
@@ -2,7 +2,7 @@ import { api } from "@/api/axios";
 import { ContentType } from "@/type/content";
 
 export const getContents = async () => {
-  const res = await api.get<ContentType>("/contents");
+  const res = await api.get<ContentType[]>("/contents");
   if (res.status !== 200) {
     throw new Error("getContents api 에러 발생");
   }
