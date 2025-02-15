06a80716b38fa09509392d8b3be69110d5fec9de
yeobi01 <yeobi_01@naver.com>
2025-02-15 17:59:11 +0900

    add: api에 type 추가

--- src/api/content.ts
+++ src/api/content.ts
@@ -1,7 +1,8 @@
 import { api } from "@/api/axios";
+import { ContentType } from "@/type/content";
 
 export const getContents = async () => {
-  const res = await api.get("/contents");
+  const res = await api.get<ContentType>("/contents");
   if (res.status !== 200) {
     throw new Error("getContents api 에러 발생");
   }
