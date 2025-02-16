1a5bbdd0712c1ba865784c63be9e074ea482b893
yeobi01 <yeobi_01@naver.com>
2025-02-16 22:10:44 +0900

    fix: 로그아웃 방식 변경

--- src/action/auth.ts
+++ src/action/auth.ts
@@ -6,8 +6,16 @@ import { redirect } from "next/navigation";
 export async function logout() {
   const cookieStore = await cookies();
 
-  cookieStore.delete("access-token");
-  cookieStore.delete("refresh-token");
+  cookieStore.set("access-token", "", {
+    expires: new Date(0),
+    path: "/",
+    domain: ".mrdang.cs.skku.edu",
+  });
+  cookieStore.set("refresh-token", "", {
+    expires: new Date(0),
+    path: "/",
+    domain: ".mrdang.cs.skku.edu",
+  });
 
   redirect("/");
 }
