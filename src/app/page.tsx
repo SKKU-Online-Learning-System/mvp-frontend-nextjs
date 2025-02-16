26066114bcd2d0173077654b77c90cc530b9f139
yeobi01 <yeobi_01@naver.com>
2025-02-16 21:05:04 +0900

    feat: 헤더구조 변경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,28 @@
-import Image from "next/image";
 import Link from "next/link";
 import { cookies } from "next/headers";
 
 import { IoHome, IoTimeSharp, IoPerson } from "react-icons/io5";
 import { MdFileUpload } from "react-icons/md";
 
-import { Header, SearchBar, LoginButton } from "@/component/common/Header";
+import { Header } from "@/component/common/Header";
 import { SideBar, SideBarButton } from "@/component/main/SideBar";
 import { MainGridContainer } from "@/component/main/MainContent";
-import mrdang_logo from "@/asset/mrdang_logo.svg";
 
 export default async function Home() {
   const cookieStore = await cookies();
-  const accessToken = cookieStore.get("access-token");
   const refreshToken = cookieStore.get("refresh-token");
+  // const accessToken = cookieStore.get("access-token");
 
   return (
     <div>
-      <Header>
-        <Link href="/">
-          <Image src={mrdang_logo} alt="mrdang logo" />
-        </Link>
-        <SearchBar />
-        <LoginButton />
-      </Header>
+      <Header refreshToken={refreshToken} />
       <div className="flex flex-row-reverse">
         <SideBar>
           <SideBarButton icon={IoHome} text="홈" />
           <SideBarButton icon={IoTimeSharp} text="수강하던 강의" />
           <hr />
           <SideBarButton icon={IoPerson} text="내 페이지" />
-          {accessToken && refreshToken ? (
+          {refreshToken ? (
             <>
               <hr />
               <Link href="/upload">
--- src/component/common/Header/Header.tsx
+++ src/component/common/Header/Header.tsx
@@ -1,8 +1,27 @@
-export function Header({ children }: { children: React.ReactNode }) {
+import Link from "next/link";
+import Image from "next/image";
+
+import {
+  SearchBar,
+  LoginButton,
+  LogoutButton,
+} from "@/component/common/Header";
+import mrdang_logo from "@/asset/mrdang_logo.svg";
+import { RequestCookie } from "next/dist/compiled/@edge-runtime/cookies";
+
+type Props = {
+  refreshToken?: RequestCookie;
+};
+
+export function Header({ refreshToken }: Props) {
   return (
     <>
       <div className="sticky top-0 z-10 flex flex-row items-end justify-between bg-white px-12 py-8">
-        {children}
+        <Link href="/">
+          <Image src={mrdang_logo} alt="mrdang logo" />
+        </Link>
+        <SearchBar />
+        {refreshToken ? <LogoutButton /> : <LoginButton />}
       </div>
     </>
   );
