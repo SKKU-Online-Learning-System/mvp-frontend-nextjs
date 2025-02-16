4450ea104e25cd5cc0cc8bd7be4cf1b2c32e62aa
yeobi01 <yeobi_01@naver.com>
2025-02-16 22:17:50 +0900

    fix: 하단 마진 위치 변경

--- src/component/main/MainContent/MainGridContainer.tsx
+++ src/component/main/MainContent/MainGridContainer.tsx
@@ -18,7 +18,7 @@ export function MainGridContainer() {
   }, []);
 
   return (
-    <div className="grid w-[calc(100vw-20rem)] gap-6 pr-12 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 3xl:grid-cols-6 4xl:grid-cols-8">
+    <div className="mb-12 grid w-[calc(100vw-20rem)] gap-6 pr-12 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 3xl:grid-cols-6 4xl:grid-cols-8">
       {contents?.map((content) => (
         <MainContentCard key={content.id} content={content} />
       ))}
