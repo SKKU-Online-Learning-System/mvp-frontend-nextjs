1f591e6f5db2778004bb3d773e7100c2e34284b2
yeobi01 <yeobi_01@naver.com>
2025-02-24 15:43:04 +0900

    feat: Video 페이지 좋아요 기능 추가

---
+++ src/component/video/VideoContent/VideoContentLike.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { useState } from "react";
+import { FaHeart, FaRegHeart } from "react-icons/fa";
+
+import { patchContentLike } from "@/api/content";
+
+type Props = {
+  id: number | undefined;
+  isLike: boolean | undefined;
+  likeCount: number | undefined;
+};
+
+export function VideoContentLike({ id, isLike, likeCount }: Props) {
+  const [like, setLike] = useState(isLike);
+  const onClickLike = async (event: React.MouseEvent) => {
+    event.preventDefault();
+    if (id) {
+      await patchContentLike(id);
+    }
+    setLike((prev) => !prev);
+  };
+  return (
+    <div className="flex flex-row items-center">
+      {like ? (
+        <FaHeart color="green" onClick={onClickLike} size={35} />
+      ) : (
+        <FaRegHeart color="gray" onClick={onClickLike} size={35} />
+      )}
+      <p className="ml-2 text-xl font-semibold">{likeCount}</p>
+    </div>
+  );
+}
