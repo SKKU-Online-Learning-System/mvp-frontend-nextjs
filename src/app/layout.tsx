134eef44c4d9fd06b8bf03281f6cfe887ba9f7c5
Min-swo <msms0226@g.skku.edu>
2025-03-19 17:55:31 +0900

    fix: 모바일 반응형을 위한 스케일 조정(임시 조치)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ export const metadata: Metadata = {
 
 export const viewport: Viewport = {
   width: "device-width",
-  initialScale: 0.5,
+  initialScale: 0.75,
   maximumScale: 1,
   userScalable: false,
 };
