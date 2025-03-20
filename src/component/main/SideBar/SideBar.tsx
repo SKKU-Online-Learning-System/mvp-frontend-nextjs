ac1c6be560034308dea6c0060fede8bc969ef363
Min-swo <msms0226@g.skku.edu>
2025-03-20 17:45:17 +0900

    hotfix: 업로드 글자 깨짐 수정

--- src/component/main/SideBar/SideBar.tsx
+++ src/component/main/SideBar/SideBar.tsx
@@ -1,6 +1,6 @@
 export function SideBar({ children }: { children: React.ReactNode }) {
   return (
-    <div className="fixed left-0 mt-4 flex w-[8rem] flex-col gap-5">
+    <div className="fixed left-0 mt-4 flex w-[10rem] flex-col gap-5 max-sm:w-[8rem]">
       {children}
     </div>
   );
