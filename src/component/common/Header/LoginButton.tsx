68d51e4fc1ed2b4bbb819a50371a3039bd0dd554
Min-swo <msms0226@g.skku.edu>
2025-03-20 17:44:03 +0900

    hotfix: 로그인/로그아웃 버튼 글자 깨짐 수정

--- src/component/common/Header/LoginButton.tsx
+++ src/component/common/Header/LoginButton.tsx
@@ -4,7 +4,7 @@ export function LoginButton() {
   return (
     <Link
       href="https://login.skku.edu/?retUrl=i0u4a8g61ure5516k3z6"
-      className="flex h-12 w-[6rem] cursor-pointer items-center justify-center rounded-xl border px-4 font-bold text-green-800 max-sm:text-sm min-[768px]:max-[845px]:text-sm"
+      className="flex h-12 w-[7.4rem] cursor-pointer items-center justify-center rounded-xl border px-4 font-bold text-green-800 max-sm:text-sm min-[768px]:max-[845px]:text-sm"
     >
       로그인
     </Link>
