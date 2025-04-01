68077162dc21097ef79ea277b25f59e7d73a1355
Kim Sang Yeob <73064401+yeobi01@users.noreply.github.com>
2025-04-01 12:07:25 +0900

    feat: hover 및 선택시 디자인 추가 (#33)

--- src/component/main/MainContent/ContentCategory.tsx
+++ src/component/main/MainContent/ContentCategory.tsx
@@ -9,17 +9,17 @@ type Props = {
 export function ContentCategory({ category }: Props) {
   return (
     <div className="flex w-full flex-col gap-2">
-      <div className="flex flex-row flex-wrap gap-4">
+      <div className="flex flex-row flex-wrap gap-2">
         <Link
           href={`/`}
-          className={`${category == null ? "text-green-700" : ""} text-[1.1rem] font-semibold text-gray-500`}
+          className={`${category == null ? "border-b-2 border-green-700 text-green-700" : ""} px-1 text-[1.1rem] font-semibold text-gray-500 hover:border-b-2 hover:border-green-700 hover:text-green-700`}
         >
           #전체
         </Link>
         {Object.keys(categoryInfo).map((key) => (
           <Link
             href={`/?category=${key}`}
-            className={`${key == category ? "text-green-700" : ""} text-[1.1rem] font-semibold text-gray-500`}
+            className={`${key == category ? "border-b-2 border-green-700 text-green-700" : ""} px-1 text-[1.1rem] font-semibold text-gray-500 hover:border-b-2 hover:border-green-700 hover:text-green-700`}
             key={key}
           >
             #{key}
