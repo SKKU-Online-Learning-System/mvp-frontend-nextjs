25cb79d23af8583070ab8040c97c503e29e39da0
Min-swo <msms0226@g.skku.edu>
2025-04-25 15:44:27 +0900

    feat: 검색기능 추가

--- src/component/main/MainContent/ContentCategory.tsx
+++ src/component/main/MainContent/ContentCategory.tsx
@@ -1,24 +1,31 @@
+"use client";
+
 import Link from "next/link";
 
 import { CategoryKey, categoryInfo } from "./category";
+import { useSearchParams } from "next/navigation";
 
 type Props = {
   category: CategoryKey | null;
 };
 
 export function ContentCategory({ category }: Props) {
+  const searchParams = useSearchParams();
+  const query = searchParams.get("query");
   return (
-    <div className="flex w-full flex-col gap-2">
-      <div className="flex flex-row flex-wrap gap-2 border-b-2">
+    <div className="flex w-full flex-col gap-3">
+      <div className="flex flex-row flex-wrap gap-3 border-b-2">
         <Link
-          href={`/`}
+          href={query ? `/?query=${query}` : "/"}
           className={`${category == null ? "border-b-2 border-green-700 text-green-700" : "opacity-60"} px-1 text-[1.1rem] font-semibold text-gray-500 hover:border-b-2 hover:border-green-700 hover:text-green-700 hover:opacity-100`}
         >
           전체
         </Link>
         {Object.keys(categoryInfo).map((key) => (
           <Link
-            href={`/?category=${key}`}
+            href={
+              query ? `/?query=${query}&category=${key}` : `/?category=${key}`
+            }
             className={`${key == category ? "border-b-2 border-green-700 text-green-700" : "opacity-60"} px-1 text-[1.1rem] font-semibold text-gray-500 hover:border-b-2 hover:border-green-700 hover:text-green-700 hover:opacity-100`}
             key={key}
           >
